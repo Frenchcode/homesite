c07056528e68cab14dff74b2a05185dbce9ce084
Ephraim Bokuma Mulenga <ephraim.bokuma-mulenga@epitech.eu>
2024-05-23 21:22:13 +0200

    [ADD] Added first screen

--- homesite/src/app/page.tsx
+++ homesite/src/app/page.tsx
@@ -1,6 +1,34 @@
+'use client'
+
+import { useEffect, useRef } from "react";
+
+
 export default function Home() {
+  const blobArea = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      console.log("We are moving");
+      const blob = blobArea.current;
+      if (blob) {
+        const half = blob.offsetWidth / 2;
+        blob.style.left = `${event.clientX - half}px`;
+        blob.style.top = `${event.clientY - half}px`;
+      }
+    };
+    document.body.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      document.body.addEventListener('mousemove', handleMouseMove);
+    }
+  }, []);
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div id="overlay"></div>
+      <div id="blob" ref={blobArea}></div>
     </main>
+    </>
   );
 }
--- homesite/tailwind.config.ts
+++ homesite/tailwind.config.ts
@@ -13,6 +13,18 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'home-black': '#020202',
+        'home-white': '#f3eff5',
+        'home-gray': '#454955',
+        'home-nature': '#72b01d',
+        'home-nature-dark': '#3f7d20',
+        'home-green-dark': '#0d2818',
+        'home-green': '#04471c',
+        'home-green-light': '#058c42',
+        'home-green-lighter': '#16db65',
+        'home-yellow': '#e4ff1a'
+      },
     },
   },
   plugins: [],
